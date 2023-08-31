555496bd5a299d0925fdf7575a359aad46dfe3f4
AlinaShatalova <lina.shatalova@yandex.ru>
2023-08-31 18:19:43 +0300

    add constants

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const SECTION_TITLE = 'Ты сегодня покормил кота?';
+
+export const CAT_NOT_LIKE = 'Котэ не одобряет?'
+export const CARD_TITLE = 'Сказочное заморское яство'
+
+export const cardState = {
+  default: 'default',
+  selected: 'selected',
+  disabled: 'disabled',
+}
